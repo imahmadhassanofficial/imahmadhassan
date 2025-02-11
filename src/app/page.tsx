658ed85cafe47f412f3b9ae6c5deacdb8b289772
imahmadhassan <imahmadhassanofficial@gmail.com>
2025-02-12 01:25:24 +0500

    Updated page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 
 export default function Home() {
   return (
-<h1>Hello World</h1>
+<h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit repellendus fuga asperiores molestias. Sunt, sed vitae. Itaque quidem adipisci aperiam accusamus, laboriosam rem commodi culpa. Perspiciatis autem aliquid omnis consequuntur.</h1>
+  
   );
 }
